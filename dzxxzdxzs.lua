07729fd4bb33d8017d98ffc7d3d8ead09969ef08
gjkdsjgs <whislzthegreat@gmail.com>
2024-06-17 19:03:26 -0400

    Update and rename xzdzxs.lua to dzxxzdxzs.lua

--- dzxxzdxzs.lua
+++ dzxxzdxzs.lua
@@ -34,7 +34,7 @@ end
 local converted_stamp = ConvertTimeStamp(update_stamp)
 local trimmed_stamp = converted_stamp:match("^%s*(.-)%s*$")
 --
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/gjkdsjgs/fdsfsdfdsfds/main/dfdzxdxzfs.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/gjkdsjgs/fdsfsdfdsfds/main/dfdzxdxzxzfs.lua"))()
 --
 local CurrentGame, RemoteEvent, SupportedGames;
 --
