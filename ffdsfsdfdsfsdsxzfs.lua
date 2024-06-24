6a5d4ad7d8c60443a14d90a52b3cf7b1d76634e0
gjkdsjgs <whislzthegreat@gmail.com>
2024-06-23 22:53:41 -0400

    Update and rename fdsfsdxzfs.lua to ffdsfsdfdsfsdsxzfs.lua

--- ffdsfsdfdsfsdsxzfs.lua
+++ ffdsfsdfdsfsdsxzfs.lua
@@ -1038,34 +1038,37 @@ function Library:Window(options)
 					GUI["37"]["PaddingBottom"] = UDim.new(0, 2);
 				end
 
-				local function IsUsingAntiAim(Player)
-					if (Player.Character.HumanoidRootPart.Velocity.Y < -5 and Player.Character.Humanoid:GetState() ~= Enum.HumanoidStateType.Freefall) or Player.Character.HumanoidRootPart.Velocity.Y < -50 then
-						return true
-					elseif Player and (Player.Character.HumanoidRootPart.Velocity.X > 35 or Player.Character.HumanoidRootPart.Velocity.X < -35) then
-						return true
-					elseif Player and Player.Character.HumanoidRootPart.Velocity.Y > 60 then
-						return true
-					elseif Player and (Player.Character.HumanoidRootPart.Velocity.Z > 35 or Player.Character.HumanoidRootPart.Velocity.Z < -35) then
-						return true
-					else
-						return false
+					local function IsUsingAntiAim(Player)
+						if (Player.Character.HumanoidRootPart.Velocity.Y < -5 and Player.Character.Humanoid:GetState() ~= Enum.HumanoidStateType.Freefall) or Player.Character.HumanoidRootPart.Velocity.Y < -50 then
+							return true
+						elseif Player and (Player.Character.HumanoidRootPart.Velocity.X > 35 or Player.Character.HumanoidRootPart.Velocity.X < -35) then
+							return true
+						elseif Player and Player.Character.HumanoidRootPart.Velocity.Y > 60 then
+							return true
+						elseif Player and (Player.Character.HumanoidRootPart.Velocity.Z > 35 or Player.Character.HumanoidRootPart.Velocity.Z < -35) then
+							return true
+						else
+							return false
+						end
 					end
-				end
 					GUI.IndicatorInitialized = true
+
+					HealthConnection = Target.Character:FindFirstChild("Humanoid"):GetPropertyChangedSignal("Health"):Connect(function()
+						GUI["25"].Text = math.floor(Target.Character:FindFirstChild("Humanoid").Health)
+						GUI["29"].Size = UDim2.new(Target.Character:FindFirstChild("Humanoid").Health / Target.Character:FindFirstChild("Humanoid").MaxHealth, 0, 1, 0)
+					end)
+	
+					AntiConnection = runService.PreRender:Connect(function()
+						if IsUsingAntiAim(Target) then
+							GUI["36"].Text = "true"
+						else
+							GUI["36"].Text = "false"
+						end
+					end)
+				else
+					if HealthConnection then HealthConnection:Disconnect() HealthConnection = nil end
+					if AntiConnection then AntiConnection:Disconnect() AntiConnection = nil end
 				end
-
-				HealthConnection = Target.Character:FindFirstChild("Humanoid"):GetPropertyChangedSignal("Health"):Connect(function()
-					GUI["25"].Text = math.floor(Target.Character:FindFirstChild("Humanoid").Health)
-					GUI["29"].Size = UDim2.new(Target.Character:FindFirstChild("Humanoid").Health / Target.Character:FindFirstChild("Humanoid").MaxHealth, 0, 1, 0)
-				end)
-
-				AntiConnection = runService.PreRender:Connect(function()
-					if IsUsingAntiAim(Target) then
-						GUI["36"].Text = "true"
-					else
-						GUI["36"].Text = "false"
-					end
-				end)
 			else
 				GUI["20"].Text = "nil"
 
