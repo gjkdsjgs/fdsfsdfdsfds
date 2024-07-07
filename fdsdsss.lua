50d9130d7b57b92be7ca7f174bd19d2a689fec25
gjkdsjgs <whislzthegreat@gmail.com>
2024-07-07 01:51:24 -0400

    Update and rename fdsfsdfdsss.lua to fdsdsss.lua

--- fdsdsss.lua
+++ fdsdsss.lua
@@ -213,7 +213,7 @@ do -- Preload
     RemoteEvent = CurrentGame.Functions.GetRemote()
 end
 --
-local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/gjkdsjgs/dsadasda/main/fdsfsdfsdfsd.lua"))()
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/gjkdsjgs/dsadasda/main/ffdsfdsdfsd.lua"))()
 --
 local Cursor = loadstring(game:HttpGet("https://raw.githubusercontent.com/gjkdsjgs/dsadasda/main/fsdfsdfs.lua"))()
 --
